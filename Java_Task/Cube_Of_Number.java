d748969b04125bde545ef97cc9ff61ee9fc2ad11
333dev <109798688+333dev@users.noreply.github.com>
2024-07-25 19:07:43 +0530

    Add files via upload

---
+++ Java_Task/Cube_Of_Number.java
@@ -0,0 +1,12 @@
+package Java_25_July_Task;
+
+public class Cube_Of_Number {
+	
+	public static void main (String args[]) {
+		
+		int a = 3 ;
+		int b = a * a * a ;
+		
+		System.out.println ("Cube Of a is : " + b) ;	
+	}
+}
