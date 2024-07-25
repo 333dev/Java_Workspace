4f7976d5e67c167c739784cbc8341d0ab21433ba
333dev <109798688+333dev@users.noreply.github.com>
2024-07-25 19:16:20 +0530

    Add files via upload

---
+++ Java_Class_Work/Java_25_July_Class_Work/Else_If.java
@@ -0,0 +1,31 @@
+package Java_Data_Type_Class_Work;
+
+public class Else_If {
+	
+	public static void main (String args[]) {
+		
+		int marks = 99;
+		
+		if (marks >= 80) {
+			
+			System.out.println("Grade A");
+		}
+		
+		else if (marks >= 60) {
+			
+			System.out.println ("Grade B");
+		}
+		
+		else if (marks >= 40) {
+			
+			System.out.println ("Grade C");
+		}
+		
+		else {
+			
+			System.out.println ("Grade D");
+		}
+		
+	}
+
+}
---
+++ Java_Class_Work/Java_25_July_Class_Work/Switch_Case.java
@@ -0,0 +1,26 @@
+package Java_Data_Type_Class_Work;
+
+public class Switch_Case {
+	
+	public static void main (String args[]) {
+		
+		int number = 3 ;
+		
+		switch (number) {
+		
+			case 1 : System.out.println ("Hindi Language") ;
+			break;
+			
+			case 2 : System.out.println ("Gujarati Language") ;
+			break;
+			
+			case 3 : System.out.println ("English Language") ;
+			break;
+			
+			default : System.out.println ("Please Enter Correct Input");
+			break;		
+		}
+		
+	}
+
+}
